c0686295835ceef1d9017e8378174b4ffc5f9c72
berokwiri150-26 <berokwiri150@gmail.com>
2026-06-23 23:05:23 +0300

    Practice through optional Lab, but no terminal outpot. Needs review.

--- server/app.py
+++ server/app.py
@@ -1,1 +1,26 @@
-existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to Flatiron Cars!"
+
+@app.route('/<string:Model>')
+def car(model):
+    existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+    if model in existing_models:
+        return 'Flatiron {model} is in our fleet'
+    else:
+        return f'Sorry, Flatiron {model} is not in our fleet'
+    
+
+if __name__ == '__name__':
+        app.run(debug=True)
+
+    
+
+
+
+
+

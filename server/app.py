1d51a247011f7835339908bacdbb9efe453c23b7
berokwiri150-26 <berokwiri150@gmail.com>
2026-06-23 23:21:36 +0300

    Got the website working well now.

--- server/app.py
+++ server/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 def home():
     return "Welcome to Flatiron Cars!"
 
-@app.route('/<string:Model>')
+@app.route('/<string:model>')
 def car(model):
     existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
     if model in existing_models:
-        return 'Flatiron {model} is in our fleet'
+        return f'Flatiron {model} is in our fleet!'
     else:
-        return f'Sorry, Flatiron {model} is not in our fleet'
+        return f'Sorry, Flatiron {model} is not in our fleet.'
     
 
-if __name__ == '__name__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run( debug=True)
 
     
 
